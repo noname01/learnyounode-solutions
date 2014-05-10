2a5305b255974f337ea0f53aa947a3649a782a2b
noname01 <hjc.noname@gmail.com>
2014-05-10 16:26:27 -0700

    reset

--- e11.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-var http = require("http");
-
-var server = http.createServer(function(request, response){
-    response.writeHeader(200, {"content-type" : "text/plain"});
-    fs.createReadStream(process.argv[3]).pipe(response);
-});
-
-server.listen(process.argv[2]);
--- e6.js
+++
@@ -1,8 +0,0 @@
-var m = require('./mymodule')
-m(process.argv[2].toString(), process.argv[3].toString(), function(err, data){
-  if(!err){
-    data.forEach(function(item){
-	console.log(item);
-    })
-  }
-)
