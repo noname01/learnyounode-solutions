5e3acb6f7968011ff90b6fd2510f0aa1f2d8d5c2
noname01 <hjc.noname@gmail.com>
2014-05-10 20:55:35 -0700

    last

--- e13.js
+++ e13.js
@@ -4,12 +4,12 @@ var url = require('url');
 var server = http.createServer(function(req, res){
     var parsedUrl = url.parse(req.url, true);
     date = new Date(parsedUrl.query.iso);
-    if(parsedUrl.pathname.toString() === '/api/parsetime'){
+    if(parsedUrl.pathname === '/api/parsetime'){
         res.writeHead(200, {'content-type': 'application/json'});
         res.end(JSON.stringify({ hour: date.getHours(),
             minute: date.getMinutes(), second: date.getSeconds() }));
     }
-    else if(parsedUrl.pathname.toString() === '/api/unixtime'){
+    else if(parsedUrl.pathname === '/api/unixtime'){
         res.writeHead(200, {'content-type': 'application/json'});
         res.end(JSON.stringify({ unixtime: date.getTime()}));
     }
