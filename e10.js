bb17318bdd8ba7da8c2dd25d88f80ebe23ce7bcc
noname01 <hjc.noname@gmail.com>
2014-05-10 17:54:24 -0700

    e10

---
+++ e10.js
@@ -0,0 +1,15 @@
+var net = require('net');
+
+function fill0(num){
+    if(num < 10) return '0' + num;
+    return num;
+}
+
+var server = net.createServer(function(socket){
+    var date = new Date();
+    var response = date.getFullYear() + '-' + fill0(date.getMonth() + 1) + '-' + fill0(date.getDate())
+        + " " + fill0(date.getHours()) + ":" + fill0(date.getMinutes()) + "\n";
+    socket.end(response);
+});
+
+server.listen(process.argv[2]);
